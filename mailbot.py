016407e4b15097867c3fd6f68b6a7a5afc62b9c0
Foxius <saintklovus@gmail.com>
2022-09-12 19:53:53 +0500

    Добавлено: Логирование, Команда /admhelp Изменено: удалены неиспользуемые модули, изменено место хранения конфига

--- mailbot.py
+++ mailbot.py
@@ -1,21 +1,19 @@
-import config
-from config import token
 import telebot
-from telebot import types
-from openpyxl import load_workbook
+import openpyxl
+import csv
 import time
 import secrets
 import string
 from datetime import date
-import pandas as pd
-from pandas import DataFrame
 from transliterate import translit
-import csv
-import openpyxl
-bot = telebot.TeleBot(config.token)
-
+from config import tok
+from config import adminID
+import os
+bot = telebot.TeleBot(tok)
+os.system("cls")
+print("[LOG] Бот Запущен")
 class Data(object):
-    def __init__(self) -> None:
+    def init(self) -> None:
         self.data:dict={}
         self.i_obj2=0
         self.i_obj1=0
@@ -28,8 +26,8 @@ data:object=Data()
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    print("start: ", message.from_user.username)
-    bot.send_message(message.chat.id, f"""Приветствую, `{message.from_user.first_name}`. Вы попали к почтовому боту ЧРТ
+    print(f"[LOG] [{message.from_user.first_name}] [start]")
+    bot.send_message(message.chat.id, f"""Приветствую, {message.from_user.first_name}. Вы попали к почтовому боту ЧРТ
     Для регистрации почты введите команду /register
     Для восстановления пароля введите команду /recovery
     Для предоставления списка проектов введите /prjlist
@@ -37,11 +35,11 @@ def start(message):
 
 @bot.message_handler(commands=['register'])
 def register_name(message):
-    print("register: ",message.from_user.username)
-
+    print(f"[LOG] [{message.from_user.first_name}] [register_name]")
     mesg = bot.send_message(message.chat.id, f"Для начала введите своё имя", parse_mode = 'Markdown')
     bot.register_next_step_handler(mesg, register_surname)
 def register_surname(message):
+    print(f"[LOG] [{message.from_user.first_name}] [register_surname]")
     mesg = bot.send_message(message.chat.id, f"Теперь введите фамилию")
     en_text = translit(message.text, language_code='ru', reversed=True)
     en_text = en_text.lower()
@@ -50,6 +48,7 @@ def register_surname(message):
     data.add({'ru_name': ru_text})
     bot.register_next_step_handler(mesg, register_id)
 def register_id(message):
+    print(f"[LOG] [{message.from_user.first_name}] [register_id]")
     en_text = translit(message.text, language_code='ru', reversed=True)
     ru_text = message.text
     en_text = en_text.lower()
@@ -60,13 +59,14 @@ def register_id(message):
     mesg = bot.send_photo(message.chat.id, photo, caption = textmesg, parse_mode='Markdown')
     bot.register_next_step_handler(mesg, register_end)
 def register_end(message):
+    print(f"[LOG] [{message.from_user.first_name}] [register_end]")
     id = message.text.upper()
     data.add({"id": id})
     alphabet = string.ascii_letters + string.digits
     password = ''.join(secrets.choice(alphabet) for i in range(20))
     text = f"""
     Я проверил, и вы действительно не имеете почты в доменной зоне radiotech\\.su и являетесь студентом ЧРТ 
-    Ваша новая почта  {data.data['name']}\\.{data.data['surname']}@radiotech\\.su  Пароль \\- ||{password}||
+    Ваша новая почта  {data.data['name']}\\.{data.data['surname']}@radiotech\\.su  Пароль \\- {password}
     Ваша почта скоро будет активна и вход по ней будет доступен\\!
     """
     chektext = f'*Проверка в базе данных учеников\\.\\.\\.*'
@@ -85,43 +85,56 @@ def register_end(message):
     groups = 'student'
     with open("registerusers.csv", "a", encoding='CP1251') as f:
         writer = csv.writer(f, delimiter=",", lineterminator="\n")
-        #writer.writerow(['username', 'password', 'firstname', 'lastname', 'groups'])
+        writer.writerow(['username', 'password', 'firstname', 'lastname', 'groups'])
         writer.writerow([minimail, password, firstname, lastname, groups])
-
-
 @bot.message_handler(commands=['recovery'])
 def recovery_mail(message):
-    print("recovery: ", message.from_user.username)
+    print(f"[LOG] [{message.from_user.first_name}] [recovery_mail]")
     mesg = bot.send_message(message.chat.id, f'Вас понял. Для заявки на восстановление почты введите свой адрес электронной почты вида name.surname@radiotech.su')
     bot.register_next_step_handler(mesg, recovery_idphoto)
 def recovery_idphoto(message):
+    print(f"[LOG] [{message.from_user.first_name}] [recovery_idphoto]")
     data.add({"mail": message.text})
     photo = open("studentid.png",'rb')
     textmesg = f'Теперь введите номер студенческого билета, чтобы найти его, посмотрите на фотографию'
     mesg = bot.send_photo(message.chat.id, photo, caption = textmesg, parse_mode='MarkdownV2' )
     bot.register_next_step_handler(mesg, recovery_accepting)
 def recovery_accepting(message):
+    print(f"[LOG] [{message.from_user.first_name}] [recovery_accepting]")
     data.add({"id": message.text})
     bot.send_message(message.chat.id, f"Заявка на восстановление пароля передана администрации. Ожидайте")
-    adminID = 1594231051
     mail = data.data['mail']
     id = data.data['id']
     bot.send_message(adminID, f"""
     *Заявка* на восстановление пароля
-    *Почта* - `{mail}`
-    *Номер* - `{id}`
+    *Почта* - {mail}
+    *Номер* - {id}
     *TGUN* - @{message.from_user.username}
-    *TGID* - `{message.from_user.id}`
+    *TGID* - {message.from_user.id}
     """, parse_mode='Markdown')
 
 @bot.message_handler(commands=['prjlist'])
 def prjlist(message):
-    print("prjlist: ", message.from_user.username)
+    print(f"[LOG] [{message.from_user.first_name}] [prjlist]")
     today = date.today()
     bot.send_message(message.chat.id, f'''Актуальные на *{today}* проекты:
     [Почтовый ТГ бот](https://t.me/mailcrt_bot)
-    [ATREI - Файлообменник с расширенными функциями для студентов техникума] (https://github.com/Foxius/atrei)
     ''', parse_mode='Markdown')
+@bot.message_handler(commands=['admhelp'])
+def helpstart(message):
+    print(f"[LOG] [{message.from_user.first_name}] [helpstart]")
+    mesg = bot.send_message(message.chat.id, f'''Вас понял, до призыва на помощь осталось только описать проблему''')
+    bot.register_next_step_handler(mesg, helpdescription)
+def helpdescription(message):
+    print(f"[LOG] [{message.from_user.first_name}] [helpdescription]")
+    data.add({"description": message.text})
+    bot.send_message(message.chat.id, f"""Наши администраторы только что получили уведомление от вас. Ожидайте""")
+    bot.send_message(adminID, f"""
+    Призыв о помощи от {message.from_user.first_name}
+    *TGUN* - @{message.from_user.username}
+    *TGID* - {message.from_user.id}
+    *Описание* - {data.data['description']}
+    """, parse_mode = "Markdown")
 bot.polling(none_stop=True, interval=0)
 try:
     bot.polling(none_stop=True)
